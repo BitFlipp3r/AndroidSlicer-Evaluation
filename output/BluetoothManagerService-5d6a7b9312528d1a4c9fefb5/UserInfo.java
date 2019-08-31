af88d7a1743bf63e76b2d6e4cfd6121fcaecdaa3
Michael Cyl <mcyl@office.datenschutz-nord.de>
2019-08-31 17:28:04 +0200

    ignore output folder

--- output/BluetoothManagerService-5d6a7b9312528d1a4c9fefb5/UserInfo.java
+++
@@ -1,9 +0,0 @@
-package android.content.pm;
-
-            = new Parcelable.Creator<UserInfo>() {
-        public UserInfo createFromParcel(Parcel source) {
-            return new UserInfo(source);
-        }
-
-    };
-
